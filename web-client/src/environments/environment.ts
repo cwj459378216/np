9ad4faf28f4fb9113cc1dc34932b9ad5ce08120c
frank <cwj459378216@163.com>
2025-01-09 20:47:53 +0800

    服务器上配置

--- web-client/src/environments/environment.ts
+++ web-client/src/environments/environment.ts
@@ -4,6 +4,6 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api'
-    // apiUrl: 'https://s699.cn:12346/api'
+    // apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'https://s699.cn:12346/api'
 };
